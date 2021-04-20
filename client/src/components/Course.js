2b41e157a523cc2b5205f3c26d25b04089bb4b06
Michael Carr <xyeres@me.com>
2021-04-20 11:49:40 -0700

    Initial Course structure

---
+++ client/src/components/Course.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Course({ course }) {
+    return (
+        <Link className="course--module course--link" to={"/courses/" + course.id}>
+            <h2 className="course--label">{course.title}</h2>
+            <h3 className="course--title">{course.description}</h3>
+        </Link>
+    )
+}
