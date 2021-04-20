fbd104a034609c534d503cf6ea3ef9ed94372af4
Michael Carr <xyeres@me.com>
2021-04-20 11:49:20 -0700

    Initial routing structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,35 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+// *Components
+import Header from './components/Header';
+import Courses from './components/Courses';
+import CreateCourse from './components/CreateCourse';
+import UpdateCourse from './components/UpdateCourse';
+import CourseDetail from './components/CourseDetail';
+import UserSignIn from './components/UserSignIn';
+import UserSignUp from './components/UserSignUp';
+import UserSignOut from './components/UserSignOut';
+import PageNotFound from './components/PageNotFound';
+
 function App() {
   return (
-    null
+    <BrowserRouter>
+      <Header />
+      <main>
+        <Switch>
+          <Route exact path="/" component={Courses}></Route>
+          <Route exact path="/courses/create" component={CreateCourse}></Route>
+          <Route exact path="/courses/:id/update" component={UpdateCourse}></Route>
+          <Route path="/courses/:id" component={CourseDetail}></Route>
+          <Route exact path="/signin" component={UserSignIn}></Route>
+          <Route exact path="/signup" component={UserSignUp}></Route>
+          <Route exact path="/signout" component={UserSignOut}></Route>
+          {/* Handle 404 requests */}
+          <Route component={PageNotFound} />
+        </Switch>
+      </main>
+    </BrowserRouter>
   );
 }
 
