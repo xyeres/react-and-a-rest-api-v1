6e3237206705665813298becdb56396ad248c9d1
Michael Carr <xyeres@me.com>
2021-04-20 11:47:38 -0700

    Build initial header state

---
+++ client/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+    return (
+        <header>
+            <div className="wrap header--flex">
+                <h1 className="header--logo"><Link to="/">Courses</Link></h1>
+                <nav>
+                    <ul className="header--signedout">
+                        <li><Link to="/signup">Sign Up</Link></li>
+                        <li><Link to="/signin">Sign In</Link></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
