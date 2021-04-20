810de437a9fade1793f275b8793882fe3bea961b
Michael Carr <xyeres@me.com>
2021-04-20 11:49:56 -0700

    Add basic messaging

---
+++ client/src/components/PageNotFound.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PageNotFound() {
+    return (
+        <div>
+            <h1>Whoops! 404 - The requested page does not exist.</h1>
+        </div>
+    )
+}
