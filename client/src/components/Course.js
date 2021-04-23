57a2911bb45ff07eb7f053253332924104be9955
Michael Carr <xyeres@me.com>
2021-04-23 09:46:07 -0700

    Fix card markup

--- client/src/components/Course.js
+++ client/src/components/Course.js
@@ -6,8 +6,8 @@ export default function Course(props) {
     const course = props.course;
     return (
         <Link className="course--module course--link" to={"/courses/" + course.id}>
-            <h2 className="course--label">{course.title}</h2>
-            <h3 className="course--title"><ReactMarkdown>{course.description}</ReactMarkdown></h3>
+            <h2 className="course--label">Course</h2>
+            <h3 className="course--title">{course.title}</h3>
         </Link>
     )
 }
